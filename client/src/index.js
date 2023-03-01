b3c2bb2a0f02f8135952da82a531485274b4ac6f
Srikanth077 <srikanthbandari2507@gmail.com>
2023-03-01 22:34:02 +0530

    index

--- client/src/index.js
+++ client/src/index.js
@@ -46,3 +46,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//Srikanth Bnadari 007
